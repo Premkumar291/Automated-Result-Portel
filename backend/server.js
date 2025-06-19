b3e1d572a08db865e61c52a02c81a0e75aa1707f
Premkumar291 <premkumar.29105@gmail.com>
2025-06-19 19:32:00 +0530

    initial-commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDb } from './dataBase/connectDb.js';
+
+dotenv.config(); // Load environment variables first
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Home page simple route
+app.get("/", (req, res) => {
+  res.send("Server Started successfully!");
+});
+
+//listening the server at start position with port 8080
+app.listen(PORT, () => {
+  connectDb();
+      console.log(`Server is running on http://localhost:${PORT}`);
+
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import LoginPage from "./components/login-page"
+import SignupPage from "./components/signup-page"
+import Dashboard from "./components/dashboard"
+
+export default function App() {
+  
+
+  return (
+    <div className="">
+      
+    </div>
+  )
+}
