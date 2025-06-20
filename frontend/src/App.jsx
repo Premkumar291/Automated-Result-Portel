a39c9631f5f6ba6724ca43fbcd528079c7c94f21
Deepakkrish20 <krishdeepak747@gmail.com>
2025-06-20 20:26:20 +0530

    completed signup and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import LoginPage from "./components/login-page"
-import SignupPage from "./components/signup-page"
-import Dashboard from "./components/dashboard"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import ForgotPassword from './ForgetPassword' 
 
-export default function App() {
-  
-
+function App() {
   return (
-    <div className="">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   )
 }
+
+export default App
+
---
+++ frontend/src/firebase.jsx
@@ -0,0 +1,16 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider, signInWithPopup };
