2ab74fa47c2404ad87772fe2e5f257be4f2496c0
Premkumar291 <premkumar.29105@gmail.com>
2025-06-20 16:54:19 +0530

    Schema Created - Routes for Authentication Are Made

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDb } from './dataBase/connectDb.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config(); // Load environment variables first
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware 
+app.use(express.json());
+app.use(cors());  
+
 //Home page simple route
 app.get("/", (req, res) => {
   res.send("Server Started successfully!");
 });
+
+app.use("/api/auth",authRoutes)
 
 //listening the server at start position with port 8080
 app.listen(PORT, () => {
