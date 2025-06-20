b28339e1b7e17cffe82bc767a735ac617e591090
Premkumar291 <premkumar.29105@gmail.com>
2025-06-20 16:54:19 +0530

    Schema Created - Routes for Authentication Are Made

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res) => {
+    res.send("Sign Up route");
+};
+
+export const login = async (req,res) => {
+    res.send("log in route");
+};
+
+export const logout = async (req,res) => {
+    res.send("log out route");
+};
