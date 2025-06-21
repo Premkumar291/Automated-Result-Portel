59992c07c406afc295178fa5df9d84879b338b72
Premkumar291 <premkumar.29105@gmail.com>
2025-06-21 21:52:03 +0530

    Added functionality and connected api with login page

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,22 @@
+// This file contains functions to handle authentication-related API calls.
+
+// It includes functions for user signup and email verification.
 export const signup = async (formData) => {
   const response = await fetch("http://localhost:8080/api/auth/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  });
+  return response.json();
+};
+
+
+// This function sends a request to verify the user's email using a verification code.
+export const login = async (formData) => {
+  const response = await fetch("http://localhost:8080/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include", // Important for cookies!
     body: JSON.stringify(formData),
   });
   return response.json();
