dfc5df810b6e7aec42657796d41882a39ab59760
Premkumar291 <premkumar.29105@gmail.com>
2025-06-21 15:13:24 +0530

    Verfication Mail - Sign up Done

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -28,6 +28,7 @@ export const signup = async (req, res) => {
             verificationTokenExpiresAt: Date.now() + 10 * 60 * 1000 // 10 minutes
         });
 
+        
         await newUser.save();
 
         // Send verification email
@@ -36,6 +37,8 @@ export const signup = async (req, res) => {
             "Verify your account",
             `Your verification code is: ${newUser.verificationToken}`
         );
+
+        
 
         // JWT authentication
         generateTokenAndSetCookie(newUser._id, res);
@@ -55,6 +58,37 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const verifyEmail = async (req, res) => {
+    const {code} = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid or expired verification code" });
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+
+        await user.save();
+
+        await sendEmail(
+            user.email,
+            "Email Verified",
+            "Your email has been successfully verified."
+        );
+
+        res.status(200).json({ message: "Email verified successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: `Error : Message ${error.message}` });
+    }
+}
 
 export const login = async (req,res) => {
     res.send("log in route");
