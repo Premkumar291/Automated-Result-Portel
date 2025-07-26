01d1efd45660551b1ef5ef5dcdeb90f0bb901af9
Premkumar291 <premkumar.29105@gmail.com>
2025-07-26 09:41:17 +0530

    env handled

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080 ;
+const URL = process.env.FRONTEND_URL;
 
 // Middleware
 app.use(express.json({ limit: '50mb' })); // Increased limit for large files
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [URL],
   credentials: true
 }));
 
